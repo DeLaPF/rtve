8b9f1c39770614e6f0815e86e12910e008a91708
DeLaPF <de.alafia@gmail.com>
2023-01-25 02:03:35 +0000

    Added release build
    Solved the performace problem (now only 6ms down from 160ms)

--- mingwBuild.bat
+++ mingwBuild.bat
@@ -1,5 +1,14 @@
-if not exist build mkdir build
-cd build
-cmake -S ../ -B . -G "MinGW Makefiles"
+:: Build Debug
+if not exist debug mkdir debug
+cd debug
+cmake -S ../ -B . -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug
 mingw32-make.exe
 cd ..
+
+:: Build Release
+if not exist release mkdir release
+cd release
+cmake -S ../ -B . -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release
+mingw32-make.exe
+cd ..
+
